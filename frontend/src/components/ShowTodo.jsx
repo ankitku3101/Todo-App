5e682eb5d36c6357194954f642895684586bade5
ankitku3101 <ankit.ku3101@gmail.com>
2024-02-27 15:38:48 +0530

    Updated ShowTodo Component

--- frontend/src/components/ShowTodo.jsx
+++ frontend/src/components/ShowTodo.jsx
@@ -1,16 +1,16 @@
 export function ShowTodo ({todos}) {
-    
     console.log(todos);
-
     return (
         <div>
-          {Object.values(todos).map((todo) => (
-            <div key={todo._id}>
-              <h1>{todo.title}</h1>
-              <h2>{todo.description}</h2>
-              <button>{todo.completed ? "Completed" : "Mark as Complete"}</button>
-            </div>
-          ))}
-        </div>
+          <>
+            {Object.keys(todos).map((todoId) => (
+              <div key={todos[todoId]._id}>
+                <h1>{console.log('h1 rendered todo', todos[todoId].title)}</h1>
+                <h2>{console.log('h2 todo desc render', todos[todoId].description)}</h2>
+                <button>{todos[todoId].completed ? "Completed" : "Mark as Complete"}</button>
+              </div>
+            ))}
+         </>
+      </div>
       );
 }
